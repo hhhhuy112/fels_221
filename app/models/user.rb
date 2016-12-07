94fcd1d371990056d53baaea04ccc97df1d2b655
buiquangthang <buiquangthangit@gmail.com>
2016-12-07 10:29:57 +0700

    Sign up feature
    fix i18n
    
    Fix indent
    
    Add flash danger
    
    Add gem file config
    
    Fix indent
    
    No cm

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  before_save {email.downcase!}
+
   has_many :activities
   has_many :lessons
+
+  validates :name, presence: true, length: {maximum: Settings.model_user.name_maximum}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: Settings.model_user.email_maximum},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+  has_secure_password
+  validates :password, presence: true, length: {minimum: Settings.model_user.password_minimum}
 end
