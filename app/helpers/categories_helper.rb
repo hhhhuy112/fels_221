85189585904a671cea9219d1442b5f07f65cea26
buiquangthang <buiquangthangit@gmail.com>
2016-12-14 10:31:50 +0700

    Show list categories
    Remove ()
    
    Fix
    
    Fix fiind_by

---
+++ app/helpers/categories_helper.rb
@@ -0,0 +1,24 @@
+module CategoriesHelper
+
+  def quantity_of_question category
+    quantity = Category.find_by_id(category.id).questions.count
+    unless quantity
+      quantity = 0
+    end
+  end
+
+  def your_result category
+    lessons = category.lessons.get_lesson_belongsto_user current_user.id
+    result = 0
+    unless lessons
+      lessons.each do |lesson|
+        result = result + lesson.learns.is_correct?.count
+      end
+    end
+    return result
+  end
+
+  def is_finished? category_id
+    quantity_of_question(category_id) == your_result(category_id) ? true : false
+  end
+end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,11 @@ class Lesson < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :learns
+
+  accepts_nested_attributes_for :learns, allow_destroy: true
+
+  scope :get_lesson_belongsto_user, -> user_id {where user_id: user_id}
+  scope :get_lesson_belongsto_category, -> user_id, category_id{
+    where user_id: user_id, category_id: category_id
+  }
 end
