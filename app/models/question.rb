f1397f7b343f27ba4ee08193001a618ad4a32527
buiquangthang <buiquangthangit@gmail.com>
2016-12-15 11:42:48 +0700

    new lesson
    nested attributes
    
    Add Settings
    
    Fix find_by
    
    Fix index
    
    user callback create activity

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,5 @@ class Question < ApplicationRecord
   scope :of_category, -> category_id {where category_id: category_id}
   scope :list_learned, -> learned_ids {where id: learned_ids}
   scope :search_content, -> keyword {where "LOWER(content) LIKE ?", "%#{keyword}%"} 
+  scope :question_not_learn, -> learned_ids {where "id NOT IN (?)", learned_ids}
 end
