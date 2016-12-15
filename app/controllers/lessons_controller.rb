3d166827770916280555f9d4ad9fddc981f209d3
buiquangthang <buiquangthangit@gmail.com>
2016-12-15 11:42:48 +0700

    new lesson
    nested attributes
    
    Add Settings
    
    Fix find_by
    
    Fix index
    
    user callback create activity

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,55 @@
+class LessonsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create]
+  before_action :correct_user, only: :show
+  
+  def index
+    @lessons = current_user.lessons.of_category(params[:category_id])
+      .paginate(page: params[:page]).per_page Settings.lessons_controller.per_page
+  end
+
+  def show
+    @results = @lesson.learns
+  end
+
+  def new
+    category = Category.find_by id: params[:category_id]
+    learned_ids = Learn.learned_ids current_user.id, category
+    if learned_ids.empty?
+      questions = category.questions.take Settings.lessons_controller.take
+    else
+      questions = category.questions.question_not_learn(learned_ids)
+        .take Settings.lessons_controller.take
+    end
+    @lesson = category.lessons.build
+    questions.each do |question|
+      @lesson.learns.build question_id: question.id
+    end
+  end
+
+  def create
+    @lesson = Lesson.new lesson_params
+    @lesson.category_id = params[:category_id]
+    @lesson.learns.each do |learn|
+      learn.update is_correct: Learn.check_correct_answer learn.answer_id
+    end
+    if @lesson.save
+      score = @lesson.learns.is_correct.count
+      @lesson.update score: score
+      redirect_to category_lesson_path @lesson.category, @lesson
+    else
+      flash[:danger] = t "not_save_lesson"
+      render :new
+    end
+  end
+
+  private
+  
+  def lesson_params
+    params.require(:lesson).permit learns_attributes: [:question_id, :answer_id]
+  end
+
+  def correct_user
+    @lesson = Lesson.find_by id: params[:id]
+    redirect_to root_url unless current_user.lessons.exists? @lesson
+  end
+end
--- app/models/learn.rb
+++ app/models/learn.rb
@@ -1,5 +1,34 @@
 class Learn < ApplicationRecord
   belongs_to :question
-  belongs_to :answer
-  belongs_to :lesson
+  belongs_to :answer, optional: true
+  belongs_to :lesson, optional: true
+
+  scope :learned_ids, -> user_id, category do
+    lessons = category.lessons.of_user user_id
+    self.add_question_id lessons
+  end
+
+  scope :learned_ids_without_category, -> user_id do
+    lessons = Lesson.of_user user_id
+    self.add_question_id lessons
+  end
+
+  scope :add_question_id, -> lessons do
+    learned_ids = []
+    lessons.each do |lesson|
+      lesson.learns.is_correct.each do |learn|
+        learned_ids << learn.question_id
+      end
+    end
+    learned_ids
+  end
+
+  scope :is_correct, -> {where is_correct: true}
+  
+  def self.check_correct_answer answer_id
+    unless answer_id
+      return false
+    end
+    Answer.find_by(id: answer_id).is_correct
+  end
 end
