a631a369c463b207f01fb0d761c9a2ab763ffb46
buiquangthang <buiquangthangit@gmail.com>
2016-12-15 10:58:42 +0700

    Show cate
    Change name scope in learn model

---
+++ app/helpers/categories_helper.rb
@@ -0,0 +1,25 @@
+module CategoriesHelper
+
+  def quantity_of_question category
+    quantity = Category.find_by_id(category.id).questions.count
+    unless quantity
+      quantity = 0
+    end
+    quantity
+  end
+
+  def your_result category
+    lessons = category.lessons.get_lesson_of_user current_user.id
+    result = 0
+    if lessons
+      lessons.each do |lesson|
+        result = result + lesson.learns.is_correct?.count
+      end
+    end
+    result
+  end
+
+  def is_finished? category_id
+    quantity_of_question(category_id) == your_result(category_id)
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :questions
   has_many :lessons
+
+  scope :order_by_date, -> {order created_at: :desc}
 end
